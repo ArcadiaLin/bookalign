3ea806390d0994cba791d5e7e0b772cd38682811
root <root@Wsl-Debian12>
2026-03-04 09:46:24 +0800

    1

---
+++ epub/read_book.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import ebooklib
+from ebooklib import epub
+from wtpsplit import SaT
+from lxml import etree
+
+def read_book(book_path: Path) -> epub.EpubBook:
+    return epub.read_epub(book_path)
+
+def get_document(book: epub.EpubBook) -> list[epub.EpubHtml]:
+    return [item for item in book.get_items() if item.get_type() == ebooklib.ITEM_DOCUMENT]
+
+
+
+if __name__ == "__main__":
+    sat = SaT("sat-31").to('cuda')
+    book_path = Path("../books/金閣寺 (三島由紀夫) (Z-Library).epub")
+    book = read_book(book_path)
+    documents = get_document(book)
+    test_xpath = "//p/text()"
+    # print(documents[7].content.decode("utf-8"))
+    tree = etree.HTML(documents[7].content)
+    result = tree.xpath(test_xpath)
+    whole_text = "".join(result)
+    print(sat.split(whole_text))
+    # print(whole_text)
+    # print(result)
+    # print(documents[7].content.decode("utf-8"))
